f9af068d5d41db183129440df4b0601de4a5f543
luckygit2001 <luckygit2001@gmail.com>
2023-02-27 11:57:46 +0530

    calcu

---
+++ index.js
@@ -0,0 +1,27 @@
+//1.display all number and operator other than ac inside input with id =result
+//when we click on it
+
+function display(num){
+    result.value+=num
+}
+
+//1.clear all content from input when we clicl Ac
+
+function clearAll(){
+    result.value=""
+
+}
+
+
+//2.evaluate expression using eval
+
+
+function Answer(){
+    result.value=eval(result.value)
+
+}
+
+function remove(){
+    
+}
+
